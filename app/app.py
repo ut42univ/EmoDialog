7802c4df09560ae8f9f410d77ddb81ac5570cfd6
Takuya Uehara <bb38122012@ms.nagasaki-u.ac.jp>
2024-07-28 17:54:12 +0900

    bit fixed

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 # app.py
 from flask import Flask
 from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
 from views import (
